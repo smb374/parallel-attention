612c516dc4b23094c8ff7b7fd9693848b251a03d
Po-Yeh Chen <poyehchen@cs.nycu.edu.tw>
2025-05-20 07:54:06 +0800

    Add clang stuff.

--- attention.c
+++ attention.c
@@ -3,9 +3,11 @@
 //
 #include <immintrin.h>
 #include <math.h>
+#include <stdalign.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/param.h>
 #include <time.h>
 
